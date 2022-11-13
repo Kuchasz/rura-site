251839df34be39f9241ef52ae00a68f0a9c13553
Kuchasz <kuchasz1@o2.pl>
2022-11-13 17:33:27 +0100

    format date changes

--- components/date-added.tsx
+++ components/date-added.tsx
@@ -1,9 +1,11 @@
 import Icon from "@mdi/react";
 import { mdiClockTimeFourOutline } from "@mdi/js";
 
-export const DateAdded = ({ date }: { date: string }) => (
+const formatDate = (date: Date) => `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
+
+export const DateAdded = ({ date }: { date: Date }) => (
     <div className="flex items-center">
         <Icon className="text-orange-500" size={1} path={mdiClockTimeFourOutline}></Icon>
-        <span className="ml-2 font-semibold">{date.toLocaleDateString("pl")}</span>
+        <span className="ml-2 font-semibold">{formatDate(date)}</span>
     </div>
 );
