88886ec4ebfb0cfb5d2b51a04f293ad2001339b2
Kuchasz <kuchasz1@o2.pl>
2022-11-13 17:30:15 +0100

    packages changes

--- components/anchor.tsx
+++ components/anchor.tsx
@@ -1,14 +1,13 @@
+import { ReactNode } from "react";
+
 type AnchorProps = {
-    children: React.ReactNode;
+    children: ReactNode;
     href: string;
     className?: string;
 };
 
 export const Anchor = ({ children, href, className }: AnchorProps) => (
-    <a
-        href={href}
-        className={`border py-2 px-4 flex border-gray-200 hover:bg-orange-500 hover:text-white ${className}`}
-    >
+    <a href={href} className={`border py-2 px-4 flex border-gray-200 hover:bg-orange-500 hover:text-white ${className}`}>
         {children}
     </a>
 );
