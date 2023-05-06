c579b43e126ba7f29d219ae6772d4f29e42bbbcb
Kuchasz <kuchasz1@o2.pl>
2023-05-06 17:27:46 +0200

    logos update

--- components/footer.tsx
+++ components/footer.tsx
@@ -43,19 +43,19 @@ export const Footer = () => {
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-innergy-racing-team.png"></img>
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-kocierz.png"></img>
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-gmina-lekawica.png"></img>
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-gmina-porabka.png"></img>
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-gmina-porabka.png"></img> */}
                 </div>
                 <div className="text-zinc-700 font-semibold text-2xl mt-20">SPONSORZY</div>
                 <div className="h-1 w-10 bg-orange-500 my-6"></div>
                 <div className="max-w-6xl flex flex-col md:flex-row flex-wrap justify-center items-center">
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-maspex.png"></img>
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-ceratizit.png"></img>
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-maspex.png"></img> */}
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-ceratizit.png"></img> */}
                     {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-decathlon.png"></img> */}
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-eurowafel.png"></img>
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-czaniecki-makaron.png"></img>
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-czaniecki-makaron.png"></img> */}
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-agropunkt.png"></img>
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-pbw.png"></img>
-                    <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-pkp-cargo-service.png"></img>
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-pbw.png"></img> */}
+                    {/* <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-pkp-cargo-service.png"></img> */}
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-power-of-science.png"></img>
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-ravelo.png"></img>
                     <img className="scale-50 -m-4 md:-m-8" src="/assets/partners/logo-euro-stempel.png"></img>
