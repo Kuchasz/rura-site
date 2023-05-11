8991088ba9ae31c652dc2e8dc87b38248206fad2
Kuchasz <kuchasz1@o2.pl>
2023-05-11 20:12:27 +0200

    description start list changed

--- pages/start-list.tsx
+++ pages/start-list.tsx
@@ -25,12 +25,12 @@ const ListaStartowa = ({
     return (
         <>
             <Head>
-                <title>Lista zawodników</title>
+                <title>Lista startowa</title>
             </Head>
             <div className="flex h-full py-16 flex-1 items-center justify-center">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Lista zawodników</h1>
-                    <h2>Ostateczna lista zawodników zawierająca numery startowe i godziny startu pojawi się 11.05.</h2>
+                    <h2>Lista startowa z numerami startowymi i wyznaczonymi godzinami startu.</h2>
                     {registrationSystemStatus !== "down" ? (
                         <Table headers={headers} getKey={p => p.name + p.lastName} rows={result}>
                             <Table.Item render={(r: ItemsType) => <div className="text-center">{r.bibNumber}</div>}></Table.Item>
