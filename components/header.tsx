f05f63b56ff6847fee115554624fecff6b0593f8
Kuchasz <kuchasz1@o2.pl>
2023-05-15 01:25:02 +0200

    starts tracking url removed

--- components/header.tsx
+++ components/header.tsx
@@ -84,19 +84,19 @@ export const Header = () => {
                                 }
                             />
                         </div>
-                        <Link
+                        {/* <Link
                             href="https://app.rura.cc/timer/m/15"
                             target="_blank"
                             className="text-sm md:text-base transition-colors uppercase cursor-pointer text-center hover:bg-white hover:text-orange-500 font-bold rounded-full px-4 py-1"
                         >
                             ŚLEDZENIE STARTów
-                        </Link>
+                        </Link> */}
                         <Link
                             href="https://app.rura.cc/result/15"
                             target="_blank"
                             className="text-sm md:text-base transition-colors mx-2 md:mx-5 uppercase cursor-pointer text-center bg-orange-500 hover:bg-white hover:text-orange-500 font-bold rounded-full px-4 py-1"
                         >
-                            WYNIKI LIVE!
+                            WYNIKI
                         </Link>
                         {/* <Link href="/rejestracja"
                             className="text-sm md:text-base transition-colors mx-2 md:mx-5 uppercase cursor-pointer text-center bg-orange-500 hover:bg-white hover:text-orange-500 font-bold rounded-full px-4 font-mono py-1"
