21f45c96260869b5f72bb832d825d6ed7b79f5bb
Kuchasz <kuchasz1@o2.pl>
2023-05-18 01:55:05 +0200

    start list url hidden

--- pages/zdjecia.tsx
+++ pages/zdjecia.tsx
@@ -26,10 +26,10 @@ function Zdjecia({ directories }: { directories: Directory[] }) {
                 <div className="flex flex-wrap">
                     {directories.length !== 0
                         ? directories.map((d) => (
-                              <Link key={d.dir} href={`zdjecia/${d.dir}`} className="relative w-full md:w-1/3 cursor-pointer h-96">
+                              <Link key={d.dir} href={`zdjecia/${d.dir}`} className="relative w-full overflow-hidden md:w-1/3 cursor-pointer h-96">
                                   <img className="absolute z-[-10] top-0 w-full h-full object-center object-cover" src={d.items[0].big} />
                                   <div className="bg-gradient-to-b from-black via-transparent to-black absolute z-[-9] top-0 w-full h-full opacity-75"></div>
-                                  <div className="group text-white font-semibold h-full flex flex-col justify-between p-4">
+                                  <div className="group text-white font-semibold h-full flex flex-col hover:bg-gray-900 transition-colors justify-between p-4">
                                       <div className="flex flex-col">
                                           <span>{d.date}</span>
                                           <span>fot. {d.author}</span>
