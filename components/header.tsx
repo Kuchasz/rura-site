cf926964785f8db7cfeca8ac4df78d36f0f61141
Kuchasz <kuchasz1@o2.pl>
2025-10-07 23:06:17 +0200

    updated results list

--- components/header.tsx
+++ components/header.tsx
@@ -99,12 +99,11 @@ export const Header = () => {
                         >
                             WYNIKI
                         </Link> */}
-                        {/* <Link
-                            href="https://events.stoprace.com/pl/1/"
+                        <Link
+                            href="/artykul/oficjalne-wyniki-2025"
                             className="text-xs px-2 md:px-4 md:text-base transition-colors uppercase cursor-pointer text-center bg-orange-500 hover:bg-white hover:text-orange-500 font-bold rounded-full py-1"
-                        >
-                            LISTA STARTOWA I WYNIKI
-                        </Link> */}
+                        >WYNIKI
+                        </Link>
                     </div>
                     <div className={classNames("flex-col ml-2 items-start", menuRevealed ? "flex sm:hidden" : "hidden")}>
                         {menuItems.map(mi => (
